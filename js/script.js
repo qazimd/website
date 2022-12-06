be73ded3ce29b55a427c7da1f2af2e2a4cb72267
Qazim Dervishi <113587189+qazimd@users.noreply.github.com>
2022-12-06 10:24:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 const header = document.querySelector("header");
+
 window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY);
+	header.classList.toggle ("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('open');
+	navbar.classList.toggle('open');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
-	navlist.classList.remove('open');
+	navbar.classList.remove('open');
 };
